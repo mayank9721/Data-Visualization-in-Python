82bfe98cb669d2a3a4aa14112d100b7768821b04
mayank9721 <51556590+mayank9721@users.noreply.github.com>
2019-09-01 16:28:46 +0530

    Update Data Visualization.py

--- Data Visualization.py
+++ Data Visualization.py
@@ -5,45 +5,19 @@
 # import pandas
 # import seaborn
 
-# In[1]:
-
-
 get_ipython().run_line_magic('pylab', 'inline')
 import pandas 
 import seaborn
-
-
-# In[2]:
-
-
 data=pandas.read_csv('Desktop/uber-raw-data-apr14.csv')
 
-
-# In[3]:
-
-
 data=pandas.read_csv('Desktop/uber-raw-data-apr14.csv')
-
-
-# In[4]:
 
 
 data
 
-
-# In[5]:
-
-
 data=pandas.read_csv('Desktop/uber-raw-data-apr14.csv')
 
-
-# In[6]:
-
-
 data
-
-
-# In[7]:
 
 
 data.tail()
@@ -51,19 +25,12 @@ data.tail()
 
 # Load CSV into memory
 
-# In[1]:
-
 
 dt='4/30/2014 23:22:00'
 
 
-# In[2]:
-
-
 dt=pandas.to_datetime(dt)
 
-
-# In[3]:
 
 
 d,t=dt.split(' ')
@@ -97,67 +64,48 @@ import pandas
 import seaborn
 
 
-# In[8]:
-
 
 dt=pandas.to_datetime(dt)
 
 
-# In[9]:
-
 
 data['Date/Time']=data['Date/Time'].map(pandas.to_datetime)
 
 
-# In[10]:
 
 
 data['Date/Time']=data['Date/Time'].map(pandas.to_datetime)
 
-
-# In[11]:
 
 
 data=pandas.read_csv('Desktop/uber-raw-data-apr14.csv')
 
 
-# In[12]:
-
 
 dt='4/30/2014 23:22:00'
 
 
-# In[13]:
 
 
 dt=pandas.to_datetime(dt)
 
 
-# In[14]:
 
 
 data['Date/Time']=data['Date/Time'].map(pandas.to_datetime)
 
 
-# In[15]:
-
-
 data.head()
 
-
-# In[16]:
 
 
 def get_dom(dt):
 
 
-# In[17]:
-
 
 def get_dom(dt)
 
 
-# In[18]:
 
 
 def get_dom(dt):
@@ -165,8 +113,6 @@ def get_dom(dt):
 
 data['dom']=data['Date/Time'].map(get_dom)
 
-
-# In[19]:
 
 
 def get_weekday(dt):
@@ -179,43 +125,30 @@ data['hour']=data['Date/Time'].map(get_hour)
 data.tail()
 
 
-# In[20]:
-
 
 def get_dom(dt):
     return dt_day
 
 data['dom']=data['Date/Time'].map(get_dom)
 
-
-# In[21]:
 
 
 dt.month
 
 
-# In[22]:
-
 
 dt.day
 
 
-# In[23]:
-
 
 dt.dom
 
-
-# In[24]:
 
 
 def get_dom(dt):
     return dt_day
 
 data['dom']=data['Date/Time'].map(get_dom)
-
-
-# In[25]:
 
 
 def get_dom(dt):
@@ -223,8 +156,6 @@ def get_dom(dt):
 
 data['dom']=data['Date/Time'].map(get_dom)
 
-
-# In[26]:
 
 
 def get_weekday(dt):
@@ -240,9 +171,6 @@ data.tail()
 # analysis
 
 # analyse the DOM
-# 
-
-# In[29]:
 
 
 hist(data.dom, bins=30, rwidth=.8, range=(0.5,30.5))
@@ -250,8 +178,6 @@ xlabel('date of the month')
 ylabel('frequency')
 title('Frequency by uber april 2014')
 
-
-# In[32]:
 
 
 #for k,rows in data.groupby('dom'):
@@ -264,31 +190,19 @@ by_date
     #print((k,len(rows)))
 
 
-# In[33]:
-
 
 plot(by_date)
-
-
-# In[34]:
 
 
 bar(range(1,3),by_date)
 
 
-# In[35]:
-
 
 bar(range[1,3],by_date)
 
 
-# In[36]:
-
 
 bar(range[1,31],by_date)
-
-
-# In[41]:
 
 
 bar(range(1,31),by_date_sorted)
@@ -299,131 +213,70 @@ title('Frequency by uber april 2014')
 ("")
 
 
-# In[38]:
-
 
 by_date_sorted=by_date.sort_values()
 by_date_sorted
 
 
-# In[39]:
-
 
 bar(range(1,31),by_date_sorted)
 
 
-# In[44]:
 
 
 hist(data.hour, bins=24, range=(.5,24)) 
-
-
-# In[46]:
 
 
 hist(data.weekday, bins=7, range=(-.5,6.5), rwidth=.8, color='#AA6666', alpha=.4)
 xticks(range(7),'mon tue wed thu fri sat sun'.split())
 
 
-# In[48]:
-
 
 hist(data.weekday, bins=7, range=(-.5,6.5), rwidth=.8, color='#AA6666', alpha=.4)
 xticks(range(7),'Mon Tue Wed Thu Fri Sat Sun'.split())
 
 
-# In[49]:
-
 
 data.groupby('hour weekday' .split()).apply(count_rows)
-
-
-# In[51]:
 
 
 hist(data.weekday, bins=7, range=(-.5,6.5), rwidth=.8, color='green', alpha=.4)
 xticks(range(7),'Mon Tue Wed Thu Fri Sat Sun'.split())
 
 
-# In[54]:
-
 
 hist(data.weekday, bins=7, range=(-.5,6.5), rwidth=.8, color='green')
 
 
-# In[57]:
 
 
 hist(data.weekday, bins=7, range =(-.5,6.5), rwidth=.8, color='green')
 xticks(range(7),'Mon Tue Wed Thu Fri Sat Sun'.split())
 
 
-# In[56]:
-
 
 data.weekday
-
-
-# In[58]:
 
 
 data.groupby('hour weekday'.split()).apply(count_rows)
 
 
-# In[59]:
-
 
 data.groupby('weekday weekday'.split()).apply(count_rows).unstack()
 
-
-# In[61]:
 
 
 hist(data.weekday, bins=7, range=(-.5,6.5), rwidth=.8, color='green')
 xticks(range(7),'mon tue wed thu fri sat sun'.split())
 
 
-# In[62]:
-
-
 plot(data['Lon'],data['Lat'],'.')
-
-
-# In[65]:
 
 
 figure(figsize=(20,20))
 plot(data['Lon'],data['Lat'],'.',ms=1,alpha=.5)
 xlim(-74.2,-73.7)
 ylim(40.7,41)
-
-
-# In[1]:
-
-
-pip install nbconvert
-
-
-# In[2]:
-
-
-pip install ipython
-
-
-# In[3]:
-
-
-ipython nbconvert — to script abc.ipynb
-
-
-# In[4]:
-
-
-ipython nbconvert — to script abc.py
-
-
-# In[ ]:
-
 
 
 
